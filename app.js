15f9d0887d24cea7df689990f6558d3ee1d5addd
Matt LaGrandeur <mattlag@users.noreply.github.com>
2020-02-14 12:27:45 -0800

    Fancy attribute editing :fax:

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 var UI = {
+	devmode: true,
 	XMLDocument: '',
 	documentName: 'test-xml-document.xml',
 	startDepth: 2,
-	indentChars: '  '
+	indentChars: '  ',
+	separator: ' = '
 };
 
 
 function main() {
-	UI.XMLDocument = loadXMLDocument(testXML.trim());
-	console.log(UI.XMLDocument);
-	loadTree();
+	if(UI.devmode) {
+		UI.XMLDocument = loadXMLDocument(testXML.trim());
+		consolelog(UI.XMLDocument);
+		loadTree();
+	} else {
+		showLoadFileDialog();
+	}
 }
 
 function loadTree(){
 	let destination = document.getElementById('wrapper');
 	destination.innerHTML = '';
 
-	let title = document.createElement('h1');
+	let title = createElem('h1');
 	title.innerText = UI.documentName;
 	document.title = UI.documentName;
 
@@ -25,9 +31,9 @@ function loadTree(){
 }
 
 function makeNode(thisNode, depth = 0, hasNoSiblings = false) {
-	// console.log(`makeNode ${thisNode.nodeName}`);
-	let node = document.createElement('div');
-	node.setAttribute('class', 'nodeWrapper');
+	// consolelog(`makeNode ${thisNode.nodeName}`);
+	let node = createElem('div', {class: 'nodeWrapper'});
+	node.onclick = closeAllDialogs;
 
 	// Node title, or just the content
 	let elem;
@@ -35,60 +41,60 @@ function makeNode(thisNode, depth = 0, hasNoSiblings = false) {
 	let toggler;
 
 	if(thisNode.nodeName === '#text'){
+		// This node is text
 		content = thisNode.nodeValue.trim();
 		if(!content && hasNoSiblings) {
-			// console.log('Empty Leaf Node found!')
+			// consolelog('Empty Leaf Node found!')
 			content = ' ';
 		}
 
 		if(content || content === ' ') {
 			content = content.replace(/\n/g, '<br>');
-			elem = document.createElement('div');
-			elem.setAttribute('class', 'textContent');
+			elem = createElem('div', {class: 'textContent'});
 			elem.innerHTML = content;
 			node.append(elem);
 		}
 		
 	} else if (thisNode.nodeName === '#comment'){
+		// This node is a comment
 		content = thisNode.nodeValue.trim();
 		if(content) {
-			elem = document.createElement('div');
-			elem.setAttribute('class', 'commentContent');
+			elem = createElem('div', {class: 'commentContent'});
 			elem.append(`<!-- ${content} -->`);
 			node.append(elem);
 			node.setAttribute('class', 'commentNode');
 		}
 
 	} else {
-		elem = document.createElement('div');
-		elem.setAttribute('class', 'nodeHeader');
+		// This node is a parent node, possibly with children
+		elem = createElem('div', {class: 'nodeHeader'});
 
 		if(thisNode.childNodes.length){
-			toggler = document.createElement('div');
-			toggler.setAttribute('class', 'toggler');
+			toggler = createElem('span', {class: 'togglerTitle'});
 			if(depth > UI.startDepth) {
-				toggler.append('⯈');
+				toggler.append(`⯈ ${thisNode.nodeName}`);
 				toggler.setAttribute('onclick', 'expand(this)');
 			} else {
-				toggler.append('⯆');
+				toggler.append(`⯆ ${thisNode.nodeName}`);
 				toggler.setAttribute('onclick', 'collapse(this)');
 			}
 
 			elem.append(toggler);
+		} else {
+			content = createElem('span', {class: 'title'});
+			content.append(thisNode.nodeName);
+			elem.append(content);
 		}
 		
-		elem.append(thisNode.nodeName);
-		elem.append(' ');
 		
 		// Attributes
 		if(thisNode.attributes){
 			for(let a=0; a<thisNode.attributes.length; a++){
+				let domNode = createElem('span', {class: 'attributeContent'});
 				let xmlAttribute = thisNode.attributes[a];
-				let domNode = document.createElement('span');
-				domNode.setAttribute('class', 'attributeContent');
+				domNode.onclick = function(event){ event.stopPropagation(); showAttributeEditDialog(xmlAttribute, domNode); };
 				domNode.setAttribute('title', `Click to edit "${xmlAttribute.name}"`);
-				domNode.append(`${xmlAttribute.name} = ${xmlAttribute.value}`);
-				domNode.onclick = function(){ openDialog(xmlAttribute, domNode); };
+				domNode.append(`${xmlAttribute.name}${UI.separator}${xmlAttribute.value}`);
 				elem.append(domNode);
 			}
 		}
@@ -97,8 +103,7 @@ function makeNode(thisNode, depth = 0, hasNoSiblings = false) {
 	}
 
 	// Child nodes
-	content = document.createElement('div');
-	content.setAttribute('class', 'nodeContent');
+	content = createElem('div', {class: 'nodeContent'});
 
 	if(depth > UI.startDepth) content.setAttribute('style', 'display:none;');
 
@@ -115,38 +120,81 @@ function makeNode(thisNode, depth = 0, hasNoSiblings = false) {
 	return node;
 }
 
-function openDialog(xmlAttribute, domNode){
-	let dialog = document.createElement('div');
-	dialog.setAttribute('id', 'dialog');
+function showAttributeEditDialog(xmlAttribute, domNode){
+	closeAllDialogs();
 
-	let lable = document.createElement('label');
+	let domNodePos = domNode.getBoundingClientRect();
+	let dialog = createElem('div', {class: 'dialog'});
+	dialog.style.top = `${domNodePos.top}px`;
+	dialog.style.left = `${domNodePos.right-1}px`;
+
+	let lable = createElem('label');
 	lable.append(xmlAttribute.name);
 
-	let input = document.createElement('input');
-	input.setAttribute('type', 'text');
+	let input = createElem('input', {type: 'text'});
 	input.setAttribute('value', xmlAttribute.value);
 
-	let confirm = document.createElement('button');
+	let confirm = createElem('button');
 	confirm.append('save');
 	confirm.onclick = function() {
-		xmlAttribute.value = input.value;
-		domNode.innerHTML = `${xmlAttribute.name} = ${xmlAttribute.value}`;
-		dialog.parentElement.removeChild(dialog);
+		xmlAttribute.value = escape(input.value);
+		domNode.innerHTML = '';
+		domNode.append(`${xmlAttribute.name}${UI.separator}${xmlAttribute.value}`);
+		closeAllDialogs();
 	};
 
-	let cancel = document.createElement('button');
+	let cancel = createElem('button');
 	cancel.append('cancel');
-	cancel.onclick = function() {
-		dialog.parentElement.removeChild(dialog);
-	};
+	cancel.onclick = closeAllDialogs;
+
+	let mask = createElem('div', {style: `
+		background-color: white;
+		border: 0;
+		width:3px;
+		height:${domNodePos.height-2}px;
+		position:absolute;
+		top:0px;
+		left:-2px;
+	`});
 
 	dialog.append(lable);
 	dialog.append(input);
-	dialog.append(document.createElement('br'));
+	dialog.append(createElem('br'));
 	dialog.append(confirm);
 	dialog.append(cancel);
+	dialog.append(mask);
+
+	domNode.setAttribute('class', 'attributeContentActive');
 
 	document.body.append(dialog);
+}
+
+function escape(value = ''){
+	value = value.replace(/&/g, '&amp;');
+	value = value.replace(/"/g, '&quot;');
+	value = value.replace(/</g, '&lt;');
+
+	return value;
+}
+
+function unescape(value = ''){
+	value = value.replace(/&quot;/g, '"');
+	value = value.replace(/&lt;/g, '<');
+	value = value.replace(/&amp;/g, '&');
+
+	return value;
+}
+
+function showLoadFileDialog() {
+	closeAllDialogs();
+}
+
+function closeAllDialogs(){
+	let dialogs = document.querySelectorAll('.dialog');
+	if(dialogs.length) dialogs.forEach((d) => d.parentElement.removeChild(d));
+	
+	let attrCons = document.querySelectorAll('.attributeContentActive');
+	if(attrCons.length) attrCons.forEach((a) => a.setAttribute('class', 'attributeContent'));
 }
 
 function loadXMLDocument(inputXML = ''){
@@ -175,9 +223,10 @@ function loadXMLDocument(inputXML = ''){
 }
 
 function expand(node){
+	closeAllDialogs();
 	try {
+		node.innerHTML = "⯆" + node.innerHTML.substring(1);
 		node.parentNode.parentNode.childNodes[1].style.display = 'block';
-		node.innerHTML = '⯆';
 		node.setAttribute('onclick', 'collapse(this)');
 	} catch(err) {
 		console.warn(err);
@@ -185,9 +234,10 @@ function expand(node){
 }
 
 function collapse(node){
+	closeAllDialogs();
 	try {
+		node.innerHTML = "⯈" + node.innerHTML.substring(1);
 		node.parentNode.parentNode.childNodes[1].style.display = 'none';
-		node.innerHTML = '⯈';
 		node.setAttribute('onclick', 'expand(this)');
 	} catch(err) {
 		console.warn(err);
@@ -195,13 +245,13 @@ function collapse(node){
 }
 
 function expandAll() {
-	let togglers = document.querySelectorAll('.toggler');
-	togglers.forEach((node) => expand(node));
+	closeAllDialogs();
+	document.querySelectorAll('.togglerTitle').forEach((node) => expand(node));
 }
 
 function collapseAll() {
-	let togglers = document.querySelectorAll('.toggler');
-	togglers.forEach((node) => collapse(node));
+	closeAllDialogs();
+	document.querySelectorAll('.togglerTitle').forEach((node) => collapse(node));
 }
 
 function loadFile() {
@@ -216,7 +266,7 @@ function downloadFile() {
 	let fblob = new Blob([content], {'type':ftype, 'endings':'native'});
 	let fname = UI.documentName;
 
-	let link = document.createElement('a');
+	let link = createElem('a');
 	window.URL = window.URL || window.webkitURL;
 	link.href = window.URL.createObjectURL(fblob);
 	link.download = fname;
@@ -228,8 +278,19 @@ function downloadFile() {
 	return;
 }
 
+function createElem(nodeName = 'div', attributes = {}){
+	let elem = document.createElement(nodeName);
+	for(let a in attributes) {
+		if(attributes.hasOwnProperty(a)){
+			elem.setAttribute(a, attributes[a]);
+		}
+	}
+
+	return elem;
+}
+
 function generateFormattedTextFromDOMNode(node, level = 0){
-	// console.log(`Node ${node.tagName} level ${level} has ${node.childNodes.length} children`);
+	// consolelog(`Node ${node.tagName} level ${level} has ${node.childNodes.length} children`);
 	let content = '';
 	let indent = '';
 	let text = '';
@@ -266,4 +327,8 @@ function generateFormattedTextFromDOMNode(node, level = 0){
 	}
 
 	return content;
+}
+
+function consolelog(message){
+	if(UI.devmode) console.log(message);
 }
